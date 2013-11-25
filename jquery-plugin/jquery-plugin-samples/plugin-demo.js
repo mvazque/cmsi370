126383f3039d7ee180f3966c1236fa5ea697517c
mvazque <mvazque93@gmail.com>
2013-11-25 13:44:55 -0800

    Added swivel example

---
+++ jquery-plugin/jquery-plugin-samples/plugin-demo.js
@@ -0,0 +1,29 @@
+$(function () {
+    $(".edit-in-place").inPlaceEditor({
+        change: function () {
+            console.log($(this).text());
+        }
+    });
+
+    $("#demo-select").dropdownSelect({
+        initial: "Catch the",
+
+        options: [
+            "Catch the",
+            "tiger",
+            "by the",
+            $.fn.dropdownSelect.DIVIDER,
+            "toe"
+        ],
+
+        change: function (oldSelection, newSelection) {
+            console.log("Changed from " + oldSelection + " to " + newSelection);
+        }
+    });
+
+    $(".swivel-this").swivel({
+        change: function (oldAngle, newAngle) {
+            console.log("Swiveled from " + oldAngle + " to " + newAngle);
+        }
+    });
+});
