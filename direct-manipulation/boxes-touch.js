92d10e26b400cec7340164c8250ab48965894650
dondi <dondi@lmu.edu>
2013-12-03 23:55:05 -0800

    Inline comments and notes for assignment 1114.

--- direct-manipulation/boxes-touch.js
+++ direct-manipulation/boxes-touch.js
@@ -1,4 +1,6 @@
-var	cache = {};
+var	cache = {}; // JD: Putting the cache variable here makes it globally visible.
+                //     Better to put it inside the function, so that only
+                //     the BoxesTouch code can see it.
 $(function(){
 	
 	
@@ -26,13 +28,35 @@ $(function(){
 		},
 		
 		startCreate: function (event){
+            // JD: Space before the brace, and after the "function" token.
 			$.each(event.changedTouches, function(index,touch){
 				var	cacheEntry = new Object(),
 					touchIndex = touch.identifier;
 				cacheEntry.initialX = touch.pageX;
 				cacheEntry.initialY = touch.pageY;
 				cache[touchIndex] = cacheEntry;
-				var	createdBox = '<div class="box"style="width: 0px; height: 0px; left: '+ touch.pageX +'px; top: '+ touch.pageY + 'px">' + 
+
+               // JD: Alternatively, you can define this "template" as a standalone
+               //     string at the top, then set its attributes via jQuery, e.g.:
+               //
+               //     ...
+               //     TEMP_BOX_TEMPLATE: '<div class="box"></div>';
+               //
+               //     ...
+               //
+               //     var tempBox = $(BoxesTouch.TEMP_BOX_TEMPLATE).css({
+               //         width: "0px",
+               //         height: "0px",
+               //         left: touch.pageX + "px",
+               //         top: touch.pageY + "px"
+               //     });
+               //
+               //     ...
+               //
+               //     You may find this approach to be a little more readable and
+               //     less error-prone.
+               //
+				var	createdBox = '<div class="box"style="width: 0px; height: 0px; left: '+ touch.pageX +'px; top: '+ touch.pageY + 'px">' +
 					'</div>';
 				$("#drawing-area").append(createdBox);
 				(cache[touchIndex].creatingbox) = $( "div div:last-child" );
@@ -42,7 +66,7 @@ $(function(){
 					element.addEventListener("touchend", BoxesTouch.unhighlight, false);
 				});
 			})
-			event.stopPropogation;
+			event.stopPropogation; // JD: This does nothing; you need the () parentheses :)
 		},
 		
 		/**
@@ -59,7 +83,8 @@ $(function(){
 						left: touch.pageX - target.deltaX,
 						top: touch.pageY - target.deltaY
 					});
-				
+
+                    // JD: Nice, got the hardcoding of the drawing area out.
 					area = $(target.movingBox).parent();
 					boxWidth = target.movingBox.width();
 					boxHeight = target.movingBox.height();
@@ -67,20 +92,34 @@ $(function(){
 					areaRight = areaOff.left + area.width();
 					areaBottom = areaOff.top + area.height();
 					off = $(target).offset();
-				
+
+                    // JD: Space after your "if"s please.
 					if(off.left > areaRight || off.top > areaBottom || 
 						off.left < areaOff.left - boxWidth || off.top < areaOff.top - boxHeight) {
 						$(target).removeClass("box-highlight");
 						$(target).addClass("box-deleteHighlight");
-					} else{
+					} else{ // JD: Aaand space before brace again :)
 						$(target).removeClass("box-deleteHighlight");
 						$(target).addClass("box-highlight");
 					}
 				}
 				else {
 					var	newLeft, newTop, newWidth, newHeight;
+                    // JD: You might recall that in class, we noted that there is
+                    //     some redundancy here that can be consolidated.  Bummer
+                    //     you weren't able to get to this.
 					if (touch.pageX < cache[touchIndex].initialX) {
 						newLeft = touch.pageX;
+                        // JD: Yeah, you want to surround your binary operators
+                        //     (minus in the line below) with spaces so that they
+                        //     are easier to spot.  Compare:
+                        //
+                        //     cache[touchIndex].initialX-touch.pageX
+                        //
+                        //         vs:
+                        //
+                        //     cache[touchIndex].initialX - touch.pageX
+                        //
 						newWidth = cache[touchIndex].initialX-touch.pageX;
 						if (touch.pageY < cache[touchIndex].initialY){
 							newTop = touch.pageY;
@@ -95,7 +134,7 @@ $(function(){
 						if (touch.pageY < cache[touchIndex].initialY){
 							newTop = touch.pageY;
 							newHeight = cache[touchIndex].initialY - touch.pageY;
-						} else{
+						} else{ // JD: ......OK, you know by now, right? :)
 							newTop = cache[touchIndex].initialY;
 							newHeight = touch.pageY - cache[touchIndex].initialY;
 						}
@@ -123,7 +162,7 @@ $(function(){
 					// touch.target.movingBox.
 					touch.target.movingBox = null;
 				}
-				else{
+				else{ // JD: Keep your braces and else on the same line: } else {
 					var touchIndex = touch.identifier;
 					cache[touchIndex].creatingbox.removeClass("create-highlight");
 					cache[touchIndex].creatingbox = null;
@@ -137,6 +176,10 @@ $(function(){
 		 */
 		unhighlight: function () {
 			$(this).removeClass("box-highlight");
+            // JD: Yikes, the if clause below is just way too tight.
+            //     Always stick with:
+            //
+            //     if (.....) {
 			if($(this).hasClass("box-deleteHighlight")){
 				$(this).remove();
 			}
